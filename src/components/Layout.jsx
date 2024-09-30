c01c239f29f1a57aa349fc8dfde7b0de37e86259
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2024-09-30 16:58:34 +0100

    just another commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import { useEffect} from "react"
 import {NavLink, Outlet} from "react-router-dom"
 import { useState } from "react"
 import useLocalStorage from "use-local-storage"
+import { Menu, Moon, Sun, X } from "lucide-react"
 
 export default function Layout() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
@@ -25,95 +26,32 @@ export default function Layout() {
   return (
     <div data-theme={darkTheme}>
       <header className="header">
-        {/* <p className="hero">SULEI<span className="hero-span">MAN</span></p> */}
         <NavLink to="/" onClick={menu ? toggleMenu : ""}>
           <div className="hero-container">
-            {
-              darkTheme ?
-              <img
-              src={"images/hero-dark.png"}
-              alt="Logo"
-              className="hero"
-              /> :
-              <img
-              src={"images/hero-light.png"}
-              alt="Logo"
-              className="hero"
-              />
-            }
+            <p className="hero">SULEI<span className="hero-span">MAN</span></p>
           </div>
-
         </NavLink>
         {
           windowWidth < 750 ?
           <div className="mobile-nav">
             {
               darkTheme ?
-              <img
-              className="theme-icon"
-              onClick={switchTheme}
-              src={"images/light-mode.png"}
-              alt="Theme icon"
-              /> :
-              <img
-              className="theme-icon"
-              onClick={switchTheme}
-              src={"images/dark-mode.png"}
-              alt="Theme icon"
-              />
+              <Sun onClick={switchTheme} className="theme-icon" strokeWidth={1.25}/>
+              :
+              <Moon onClick={switchTheme} className="theme-icon" strokeWidth={1.25}/>
             }
-
             {
-              !menu && !darkTheme ?
-              <img
-              className="menu"
-              onClick={toggleMenu}
-              src={"images/menu-light.png"}
-              alt="Menu"
-              /> :
-              !menu && darkTheme ?
-              <img
-              className="menu"
-              onClick={toggleMenu}
-              src={"images/menu-dark.png"}
-              alt="Menu"
-              /> :
-              menu && !darkTheme ?
-              <img
-              className="menu"
-              onClick={toggleMenu}
-              src={"images/cancel-light.png"}
-              alt="Menu"
-              /> :
-              <img
-              className="menu"
-              onClick={toggleMenu}
-              src={"images/cancel-dark.png"}
-              alt="Menu"
-              />
+              !menu ? <Menu onClick={toggleMenu} className="menu" strokeWidth={1.25}/>
+              :
+              <X onClick={toggleMenu} className="menu" strokeWidth={1.25}/>
             }
-            {/* <img
-            className="menu"
-            onClick={toggleMenu}
-            src={
-              !menu && !darkTheme ?
-              "images/menu-light.png" :
-              !menu && darkTheme ?
-              "images/menu-dark.png" :
-              menu && !darkTheme ?
-              "images/cancel-light.png" :
-              "images/cancel-dark.png"
-            }
-            alt="Menu"
-            /> */}
           </div> :
           <div className="desktop-nav">
-            <img
-            className="theme-icon"
-            onClick={switchTheme}
-            src={darkTheme ? "images/light-mode.png" : "images/dark-mode.png"}
-            alt="Theme icon"
-            />            
+            {
+            darkTheme ?
+            <Sun onClick={switchTheme} className="theme-icon" strokeWidth={1.25}/> :
+            <Moon onClick={switchTheme} className="theme-icon" strokeWidth={1.25}/>
+            }
             <NavLink className={({isActive}) => isActive ? "active-link" : ""} to="/about">
               <p>ABOUT</p>
             </NavLink>
@@ -145,3 +83,5 @@ export default function Layout() {
     </div>
   )
 }
+
+
